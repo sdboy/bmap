f787d3307739585700717b956a26a204f176d0a7
ray <jiaoyiguanggo@gmail.com>
2022-07-02 20:37:11 +0800

    修改base url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,7 @@ import { MapglComponent } from './mapgl/mapgl.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: './' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
