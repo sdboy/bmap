8b1afffc9d93b692b689cec1fbe3e9089d83fb3c
ray <jiaoyiguanggo@gmail.com>
2022-07-02 16:44:05 +0800

    初始化项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapglComponent } from './mapgl/mapgl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapglComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mapgl/mapgl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'mapgl',
+    templateUrl: './mapgl.component.html',
+    styleUrls: ['./mapgl.component.sass']
+
+})
+
+export class MapglComponent {}
